6177345c1759b2278bbf3f8e9165a0b3670903bc
mgreenberry <michaelgreenberry@gmail.com>
2021-07-19 11:48:48 +0000

    css amended for layout and structure. HTML amended to add class to paragraphs. JS amended to show menu text when pressed

---
+++ assets/js/projects.js
@@ -0,0 +1,25 @@
+/* When the user scrolls the page, execute myFunction
+Code adapted from https://www.w3schools.com/howto/howto_js_navbar_sticky.asp */
+window.onscroll = function() {myFunction()};
+// Get the navbar
+let navbar = document.getElementById("navbar");
+// Get the offset position of the navbar
+let sticky = navbar.offsetTop;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+// Ends adapted code from w3schools.com//
+/* My code
+// Change content of div/paragraph under each menu option:*/
+document.getElementById("projects").addEventListener("click", projects);
+
+// Function to change text displayed depending on which option use has clicked:
+function projects() {
+  document.getElementById('menu-text').innerHTML=`
+  <p>Current Projects and Finished Projects with pictures (slideshow) and an interactive map</p>`;
+}
