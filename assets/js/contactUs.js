2fe9a46a8c8e5c7f05e9035d458681e8cfa25142
mgreenberry <michaelgreenberry@gmail.com>
2021-07-27 16:26:48 +0000

    style.css changed to format buttons. contactUs.js changed for form and buttons. index.html amended to add formatting and id referencing

--- assets/js/contactUs.js
+++ assets/js/contactUs.js
@@ -3,9 +3,9 @@ document.getElementById("contact-us").addEventListener("click", contactUs);
 // Function to change text displayed depending on which option use has clicked:
 function contactUs() {
     document.getElementById('menu-text').innerHTML=`
-    <div class="container background">
-    <form action="post">
-        <div class="form-row">
+    <div class="container-fluid" id="menu-content">
+      <form action="post">
+          <div class="form-row">
             <div class="form-group col-md">
                 <label class="col-md link-text" for="first-name">First Name</label>
                 <input class="col-md link-text" type="text" name="fname" placeholder="First Name" id="first-name"
@@ -16,24 +16,24 @@ function contactUs() {
                 <input class="col-md link-text" type="text" name="lname" placeholder="Last Name" id="last-name"
                     required>
             </div>
-        </div>
-        <div class="form-row">
+          </div>
+          <div class="form-row">
             <div class="col-md">
                 <label class="col-md link-text" for="email">Email address</label>
                 <input class="col-md link-text" type="email" name="email" placeholder="Email Address" id="email"
                     required>
             </div>
-        </div>
-        <div class="form-group">
+          </div>
+          <div class="form-group">
             <label class="col-12 link-text" for="text">Message</label>
             <textarea class="col-12" rows="6" name="message"
                 placeholder="Please enter your question, comments or request here" id="text" required></textarea>
-        </div>
-        <div class="form-row-md link-text">
-            <input class="submit formbutton btn-info btn-reset" type="reset" value="Cancel">
-            <input class="cancel formbutton btn-primary" type="submit" value="Submit">
-        </div>
-    </form>
-  </div>
+          </div>
+          <div class="form-row-md link-text">
+            <input class="submit formbutton btn-info btn-reset centered" type="reset" value="Cancel">
+            <input class="cancel formbutton btn-primary centered" type="submit" value="Submit">
+          </div>
+      </form>
+    </div>
       `;
   }
