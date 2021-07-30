d1b4e0bec3672c406ff9020c7fc1aae4f20b701e
mgreenberry <michaelgreenberry@gmail.com>
2021-07-30 11:36:52 +0000

    project.js amended to show more information when user clicks each project photograph. services.js amended to include innerHTML for content of this page. index.html amended to allow javascript files to be loaded but commenting out those not working

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -10,19 +10,19 @@ function projects() {
       </div>
       <div id="existing-projects">
         <div class="row">
-          <div class="col-md">
+          <div class="col-md" id="project1" onclick="projectInfo1()">
             <img src="assets/images/index/cover-image1.jpg" id="project-main-image" alt="Existing Projects">
-            <p class="centered">Describing image using javascript</p>
+            <h3 class="centered">Ex. Archbishop McGrath School</h3>
           </div>
-          <div class="col-md">
+          <div class="col-md" id="project2" onclick="projectInfo2()">
             <img src="assets/images/index/cover-image2.jpg" id="project-main-image" alt="Existing Projects">
-            <p class="centered">Describing image using javascript</p>
+            <h3 class="centered">Durham Road</h3>
           </div>
         </div>   
         <div class="row">
-          <div class="col-md">
+          <div class="col-md" id="project3" onclick="projectInfo3()">
             <img src="assets/images/index/cover-image3.jpg" id="project-main-image" alt="Existing Projects">
-            <p class="centered">Describing image using javascript</p>
+            <h3 class="centered">Merthyr Street</h3>
           </div>
           <div class="col-md">
             <img src="assets/images/index/cover-image4.jpg" id="project-main-image" alt="Existing Projects">
@@ -44,3 +44,48 @@ function projects() {
     `;
   }
   
+
+  /*$(document).ready(function(){
+    $("#project1-info").click(function(){
+      $("project1").hide();
+    });
+    $("#project1-info").click(function(){
+      $("project1").show();
+    });
+  });*/
+
+  function projectInfo1() {
+      document.getElementById("project1").innerHTML = `
+      <div id="project1-info">
+      <h3 class="centered">Ex. Archbishop McGrath School</h3>
+      <img id="project-main-image" src="assets/images/index/cover-image1.jpg" alt="Ex. Archbishop McGrath School">
+            <li>Location: Brynmenyn</li>
+            <li>Client: Linc-Cymru</li>
+            <li>Contractor: Jehu</li>
+            <p>A 48 bed Care/Extra Care and 18 unit associated housing project for Linc and Jehu post-contract. All surface water taken to infiltration systems, with foul drainage taken to adoptable sewers within the neighbouring road. Dealings with DCWW included for the diversion of a pumping main serving the adjacent College and liaison with the nearby school being built at the same time to coordinate interface issues. The past history of the site left an abandoned mine shaft which was treated and capped. All foundations were rafted to guard against mining subsidence.</p>
+      </div>
+      `;
+  }
+  function projectInfo2() {
+    document.getElementById("project2").innerHTML = `
+    <div id="project2-info">
+    <h3 class="centered">Durham Road</h3>
+    <img src="assets/images/index/cover-image2.jpg" id="project-main-image" alt="Durham Road Construction">
+          <li>Location: Newport</li>
+          <li>Client: Linc-Cymru</li>
+          <li>Contractor: Jehu</li>
+          <p>A 38 four storey apartment block of timber frame construction founded on piled ground beams due to the presence of fill overlying soft alluvial deposits. Much drainage survey work was required and liaison with Railtrack and Newport land drainage to allow restricted discharge to an existing surface water culvert on the boundary of the site.</p>
+    </div>
+    `;
+}
+function projectInfo3() {
+  document.getElementById("project3").innerHTML = `
+  <div id="project3-info">
+  <h3 class="centered">Merthyr Street</h3>
+        <li>Location: Barry</li>
+        <li>Client: Linc-Cymru</li>
+        <li>Contractor: W.K. Plasterers</li>
+        <p>A four storey proprietary metal frame system build on this previously developed site. The original building, with roads to three sides, and having a part basement and adjacent to the end of a terrace presented engineering difficulties in devising a suitable piled foundation system, requiring cantilevered and stepped foundation details.</p>
+  </div>
+  `;
+}
--- assets/js/services.js
+++ assets/js/services.js
@@ -3,15 +3,37 @@ document.getElementById("services").addEventListener("click", services);
 function services() {
     document.getElementById('menu-text').innerHTML=`
     <section class="container-fluid">
-      <div id="services">
-        <div class="row">
-          <div class="col-12">
-            <h1 class="centered">Services</h1>
-          </div>
+      <div class="row">
+        <div class="col-12">
+          <h1 class="centered">Services</h1>
+          <p>Smart Associates are able to offer a full range of Civil and Structural Engineering Consultancy Services, which are not limited to the following: -</p>
         </div>
-        <div class="row">
-          <div class="col-md">SERVICES
-          </div>
+      </div>
+      <div class="row">
+        <div class="col-md">
+          <h3>Civil Engeneering</h3>
+          <ul>
+            <li>Site Inspections</li>
+            <li>Feasability Studies</li>
+            <li>Highways and Infrastructure</li>
+            <li>Drainage Designs and Hydraulic Modelling</li>
+            <li>Sustainable Drainage (SuDs) and SAB Submission</li>
+            <li>External Works</li>
+            <li>Earth and Water Retaining Structures</li>
+          </ul>
+        </div>
+        <div class="col-md">
+          <h3>Structural Engineering</h3>
+          <ul>
+            <li>Site Inspections</li>
+            <li>Structural Analysis</li>
+            <li>Structural Design and Detailing</li>
+            <li>Structural Surveys and Reports</li>
+            <li>Foundation Designs and Advice</li>
+            <li>Listed Buildings</li>
+            <li>Extensions and Alterations</li>
+            <li>New Build Developments</li>
+          </ul>
         </div>
       </div>
     </section>
